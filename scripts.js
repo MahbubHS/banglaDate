29299e70ced8db39dab8c708fad68431f9a4fa92
Saad <137253486+Oboshor@users.noreply.github.com>
2024-12-01 21:04:03 +0600

    add project

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var তারিখ = new bdDateConverter().convert(`j-m-Y`);
+document.querySelector('title').textContent =  `আজ : ${তারিখ} বঙ্গাব্দ`;
+
+var timeNowBd = setInterval(()=>{
+  var date = new bdDateConverter().convert(`j ই F, Y বঙ্গাব্দ`);
+  var day = new bdDateConverter().convert(`এখন: l`);
+  var time = new bdDateConverter().convert('A g টা i মিনিট S');
+  document.getElementById('timeNowBd').innerHTML = `<div>${day} </div><div>${time}</div>`;
+  document.getElementById('dateNowBd').textContent = date;
+},100)
