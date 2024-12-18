aa148f82b8436c64bc3d2a72076b80f04950aad1
MD Mahbub Hasan Saad <137253486+Oboshor@users.noreply.github.com>
2024-12-18 15:16:16 +0600

    line fixed

--- BdDateTime.js
+++ BdDateTime.js
@@ -37,11 +37,7 @@ function bdDateConverter(inputDate) {
   function daysIntoYear(date) {
     return (
       (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
-        Date.UTC(date.getFullYear(), 0, 0)) /
-      24 /
-      60 /
-      60 /
-      1000
+        Date.UTC(date.getFullYear(), 0, 0)) /24/60/60/1000
     );
   }
   this.totalDaysInEN = 0;
